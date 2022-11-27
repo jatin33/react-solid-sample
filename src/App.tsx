c24b68c3d50052b0fed356d5cf4dba82776b45fa
Jatin Panjwani <jatinpanjwani12@gmail.com>
2022-11-27 17:39:39 +0000

    Update 18 files

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
 import "./App.css";
-import { DIP } from "./principles/DIP";
-import { LSP } from "./principles/LSP";
-import { OCP } from "./principles/OCP";
-import { SRP } from "./principles/SRP";
+import { SRP } from "./principles/srp";
 
 function App() {
   return (
     <div className="flex min-w-full h-full justify-center items-center p-8">
-      {/* <SRP /> */}
-      {/* <OCP /> */}
-      {/* <LSP /> */}
-      <DIP />
+      <SRP />
     </div>
   );
 }
--- src/principles/srp/index.tsx
+++ src/principles/srp/index.tsx
@@ -1,9 +1,4 @@
-import axios from "axios";
-import { useEffect, useMemo, useState } from "react";
-import { Product } from "./product";
-import { Rating } from "react-simple-star-rating";
 import { Bad } from "./bad";
-import { Good } from "./good";
 
 export function SRP() {
   return <Bad />;
